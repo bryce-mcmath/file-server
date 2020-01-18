77b70ddc262e64e91e0f0223f10673f8c5cf009d
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2020-01-17 16:59:44 -0800

    Removing garbage file used for testing

--- test.js
+++
@@ -1,8 +0,0 @@
-let jsonString = '{"a":1, "b":2, "foo":"bar"}'; // string version of a JS Object
-let json = JSON.parse(jsonString);
-console.log(jsonString); // a string, of course :)
-console.log(json);
-delete json.foo;
-console.log(json);
-jsonString = JSON.stringify(json);
-console.log(jsonString);
